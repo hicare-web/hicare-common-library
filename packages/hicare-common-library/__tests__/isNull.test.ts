87d9bd3c46ef1875c2a5421ebbecb2bb876f2b55
hyun <ysk30430@naver.com>
2024-06-22 10:38:01 +0900

    feat : RDGTDL-207
    1. isNull 함수 추가
    2. isNulll test 추가

---
+++ packages/hicare-common-library/__tests__/isNull.test.ts
@@ -0,0 +1,18 @@
+import { isNull as _isNull } from 'lib/utility/internal/isNull';
+import { describe, it } from 'node:test';
+import { expect } from 'vitest';
+
+describe('isNull', () => {
+    it('should return true if the value is null', () => {
+        expect(_isNull(null)).toBe(true);
+    });
+    it('should return false if the value is not null', () => {
+        expect(_isNull(undefined)).toBe(false);
+        expect(_isNull(0)).toBe(false);
+        expect(_isNull('')).toBe(false);
+        expect(_isNull([])).toBe(false);
+        expect(_isNull({})).toBe(false);
+        expect(_isNull(new Date())).toBe(false);
+        expect(_isNull(new Error())).toBe(false);
+    });
+});
