084a52cf8214d972f47697b2982555740c104f6d
hyun <ysk30430@naver.com>
2024-06-22 15:37:02 +0900

    feat : RDGTDL-207
    1. isNull > isNullish 체크하는것으로 수정

---
+++ packages/hicare-common-library/__tests__/isNullish.test.ts
@@ -0,0 +1,17 @@
+import { isNullish as _isNullish } from 'lib/utility/isNullish';
+import { describe, expect, it } from 'vitest';
+
+describe('isNull', () => {
+    it('should return true if the value is null or undefined', () => {
+        expect(_isNullish(null)).toBe(true);
+        expect(_isNullish(undefined)).toBe(true);
+    });
+    it('should return false if the value is not null | undefined', () => {
+        expect(_isNullish(0)).toBe(false);
+        expect(_isNullish('')).toBe(false);
+        expect(_isNullish([])).toBe(false);
+        expect(_isNullish({})).toBe(false);
+        expect(_isNullish(new Date())).toBe(false);
+        expect(_isNullish(new Error())).toBe(false);
+    });
+});
