16b39b2af06c3dde62475ade3afd11c0ae219462
hyun <ysk30430@naver.com>
2024-06-25 15:40:02 +0900

    fix :RDGTDL-207
    1. test코드 매개변수 순서 수정하여 not container 오류 수정

--- packages/hicare-common-library/__tests__/isIncludedIn.test.ts
+++ packages/hicare-common-library/__tests__/isIncludedIn.test.ts
@@ -3,10 +3,11 @@ import { describe, expect, it } from 'vitest';
 
 describe('isIncludedIn', () => {
     it('배열 안에 찾는 값이 있으면 true', () => {
-        expect(isIncludedIn([1, 2, 3], 2)).toBe(true);
+        expect(isIncludedIn(2, [1, 2, 3])).toBe(true);
+        expect(isIncludedIn('b', ['a', 'b', 'c'])).toBe(true);
     });
 
     it('배열 안에 찾는 값이 없으면 false', () => {
-        expect(isIncludedIn([1, 2, 3], 4)).toBe(false);
+        expect(isIncludedIn(4, [1, 2, 3])).toBe(false);
     });
 });
