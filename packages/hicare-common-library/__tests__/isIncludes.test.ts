c95b801aa4fac23cc79c6e6a7ace1e406e61d99a
hyun <ysk30430@naver.com>
2024-06-25 14:59:15 +0900

    feat:RDGTDL-207
    1. isIncludedIn 추가

---
+++ packages/hicare-common-library/__tests__/isIncludes.test.ts
@@ -0,0 +1,12 @@
+import { isIncludedIn } from 'lib/utility/isIncludeedIn';
+import { describe, expect, it } from 'vitest';
+
+describe('isIncludedIn', () => {
+    it('배열 안에 찾는 값이 있으면 true', () => {
+        expect(isIncludedIn([1, 2, 3], 2)).toBe(true);
+    });
+
+    it('배열 안에 찾는 값이 없으면 false', () => {
+        expect(isIncludedIn([1, 2, 3], 4)).toBe(false);
+    });
+});
