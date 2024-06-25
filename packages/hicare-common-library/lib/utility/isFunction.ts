6ea26a706b72f817b0a173850f80ec0c392a856c
hyun <ysk30430@naver.com>
2024-06-25 10:42:48 +0900

    refac : RDGTDL-207
    1. isFunction 수정

--- packages/hicare-common-library/lib/utility/isFunction.ts
+++ packages/hicare-common-library/lib/utility/isFunction.ts
@@ -1,9 +1,7 @@
-type DefinitelyFunction<T> = Extract<T, Function> extends never ? Function : Extract<T, Function>;
-
 /**
- * 주어진 데이터가가 함수인지 확인하는 함수
+ * 주어진 변수가 함수인지 확인하는 함수
  *
- * @param data - 확인할 데이터
+ * @param data - 확인할 변수
  * @returns 데이터가 함수이면 true, 아니면 false.
  * @signature
  *  isFunction(data)
@@ -12,10 +10,16 @@ type DefinitelyFunction<T> = Extract<T, Function> extends never ? Function : Ext
  *  isFunction(Math.round) // => true
  *  isFunction(function * Any() {}) //=> true
  *  isFunction(class Any{}) // => true
+ *
  *  isFunction('string') //=> false
+ * isFunction(123) //=> false
+ * isFunction({}) //=> false
+ * isFunction([]) //=> false
+ * isFunction(null) //=> false
  *
  * @category Guard
  */
-export function isFunction<T>(data: Function | T): data is DefinitelyFunction<T> {
+
+export function isFunction<T>(data: T): data is T & Function {
     return typeof data === 'function';
 }
