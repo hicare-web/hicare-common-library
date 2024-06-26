661da438a88a83202a3804ec261dd43ab11dbe81
hyun <ysk30430@naver.com>
2024-06-26 10:19:08 +0900

    feat : RDGTDL - 207
    1. allPass함수 추가, test코드 작성

---
+++ packages/hicare-common-library/__tests__/allPass.test.ts
@@ -0,0 +1,31 @@
+import { allPass } from 'lib/utility/allPass';
+import { describe, expect, it } from 'vitest';
+import { isNullish } from 'lib/utility/isNullish';
+import { isString } from 'lib/utility/isString';
+import { isArray } from 'lib/utility/isArray';
+
+describe('allPass', () => {
+    const isDivisibleBy3 = (x: number) => {
+        return x % 3 === 0;
+    };
+    const isDivisibleBy4 = (x: number) => x % 4 === 0;
+    const fns = [isDivisibleBy3, isDivisibleBy4];
+    it('모든 함수에 대해 data가 조건을 만족하면 true 반환', () => {
+        expect(allPass(12, fns)).toBe(true);
+        expect(allPass(24, [isDivisibleBy4, isDivisibleBy3])).toBe(true);
+        expect(allPass(undefined, [isNullish])).toBe(true);
+        expect(allPass(null, [isNullish])).toBe(true);
+        expect(allPass(null, [isNullish, isDivisibleBy3])).toBe(true);
+        expect(allPass('', [isString])).toBe(true);
+        expect(allPass([1, 2, 3], [isArray])).toBe(true);
+        expect(allPass([4, 5], [isArray, isString])).toBe(false);
+    });
+
+    it('함수 중 하나라도 조건을 만족시키지 못하면 false 반환', () => {
+        expect(allPass(8, fns)).toBe(false);
+        expect(allPass(0, [isNullish])).toBe(false);
+        expect(allPass(0, [isNullish, isNullish])).toBe(false);
+        expect(allPass([1, 2, 3], [isNullish])).toBe(false);
+        expect(allPass('', [isNullish, isDivisibleBy3])).toBe(false);
+    });
+});
