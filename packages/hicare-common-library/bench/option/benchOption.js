9a807b1ecc307ae78d15a97fb945fd59f3ec996a
BuYoungLee <leebu18@gmail.com>
2024-07-03 15:45:02 +0900

    feat(add sort bench)

--- packages/hicare-common-library/bench/option/benchOption.js
+++ packages/hicare-common-library/bench/option/benchOption.js
@@ -1,2 +1,3 @@
+export const mapBenchOption = { time: 1000, warmupTime: 2000, warmupIterations: 20 };
 
-export const mapBenchOption =  { time: 1000, warmupTime: 2000, warmupIterations: 20 };
+export const sortBenchOption = { time: 1000, warmupTime: 2000, warmupIterations: 20 };
