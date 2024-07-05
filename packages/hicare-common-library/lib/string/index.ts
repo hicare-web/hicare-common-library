1a1d8759ca89af4dec8f31db594c5e84a79d9d1b
buyong <leebu18@gmail.com>
2024-07-05 09:02:29 +0900

    feat(stringCase 추가)

---
+++ packages/hicare-common-library/lib/string/index.ts
@@ -0,0 +1,32 @@
+import { titleCase } from './titleCase';
+
+import {
+    camelCase,
+    capitalCase,
+    constantCase,
+    dotCase,
+    kebabCase,
+    noCase,
+    pascalCase,
+    pascalSnakeCase,
+    pathCase,
+    sentenceCase,
+    snakeCase,
+    trainCase,
+} from './changeCase';
+
+export const stringCase = {
+    camelCase,
+    capitalCase,
+    constantCase,
+    dotCase,
+    kebabCase,
+    noCase,
+    pascalCase,
+    pascalSnakeCase,
+    pathCase,
+    sentenceCase,
+    snakeCase,
+    titleCase,
+    trainCase,
+};
